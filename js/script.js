e96e491f8b475145f4fcfe8dcf49f87349d85d64
Diptamoy Mitra <91617575+Diptamoy-Mitra@users.noreply.github.com>
2023-10-31 00:02:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,7 +24,8 @@ form.addEventListener("submit", (e) => {
 
 async function shortenUrl(url) {
   try {
-    const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+    //https://api.shrtco.de/v2/shorten?
+    const res = await fetch(`https://ismaelc-bitly.p.rapidapi.com/v3/shorten?url=${url}`); // https://ismaelc-bitly.p.rapidapi.com/v3/shorten
     const data = await res.json();
     const newUrl = document.createElement("div");
     newUrl.classList.add("item");
